5b6b1e331601594d5ededd0adae5302bc1ee56fb
Aryan_Sinha <83386109+NomeCoder@users.noreply.github.com>
2025-09-23 23:01:03 +0530

    Update gnn_apk_model.py

--- gnn_apk_model.py
+++ gnn_apk_model.py
@@ -1,52 +1,32 @@
 import numpy as np
 import tensorflow as tf
-
-import spektral
-dataset=spektral.datasets.citation.Citation(name='cora')
-graph=dataset[0]
-
-train_masks = dataset.mask_tr
-val_masks   = dataset.mask_va
-test_masks  = dataset.mask_te
+from spektral.datasets import citation
 
 
-num_apks = 200
-num_features = 13
-num_classes = 2
-hidden_units = 32
-dropout_rate = 0.2
-epochs = 100
-
-np.random.seed(42)
-X_int = np.random.randint(0, 2, (num_apks, num_features))
+dataset = citation.Cora()
+graph = dataset[0]
 
 
-degree_feat = X_int.sum(axis=1, keepdims=True)
-X_int = np.hstack([X_int, degree_feat])
-X = tf.constant(X_int.astype(np.float32))
+X = tf.convert_to_tensor(graph.x, dtype=tf.float32)  #
+y_onehot = tf.convert_to_tensor(graph.y, dtype=tf.float32)  
+A = tf.convert_to_tensor(graph.a.todense(), dtype=tf.float32)  
+
+y = tf.argmax(y_onehot, axis=1, output_type=tf.int64)  
+
+num_nodes, num_features = X.shape
+num_classes = y.numpy().max() + 1  # 7
 
 
-y = np.zeros(num_apks, dtype=np.int32)
-y[(X_int[:, 2] == 1) | (X_int[:, 3] == 1)] = 1
-y = tf.constant(y, dtype=tf.int32)
-
-A = np.zeros((num_apks, num_apks), dtype=np.float32)
-for i in range(num_apks):
-    for j in range(i, num_apks):
-        if np.any(X_int[i] & X_int[j]):  
-            A[i, j] = 1
-            A[j, i] = 1
-np.fill_diagonal(A, 1)
-
+A = A.numpy()
 D_inv_sqrt = np.diag(1.0 / np.sqrt(A.sum(axis=1)))
 A_norm = D_inv_sqrt @ A @ D_inv_sqrt
-A_norm = tf.constant(A_norm, dtype=tf.float32)
+A_norm = tf.convert_to_tensor(A_norm, dtype=tf.float32)
 
-train_mask = np.zeros(num_apks, dtype=bool)
-train_mask[:int(0.8*num_apks)] = True
-np.random.shuffle(train_mask)
-train_indices = np.where(train_mask)[0]
-test_indices = np.where(~train_mask)[0]
+
+train_mask = dataset.mask_tr
+test_mask = dataset.mask_te
+train_indices = tf.constant(np.where(train_mask)[0], dtype=tf.int64)
+test_indices = tf.constant(np.where(test_mask)[0], dtype=tf.int64)
 
 
 class SimpleGCN(tf.keras.Model):
@@ -64,42 +44,39 @@ class SimpleGCN(tf.keras.Model):
         Z = tf.matmul(A, self.dense2(H))
         return Z
 
-model = SimpleGCN(hidden_units=hidden_units, num_classes=num_classes, dropout_rate=dropout_rate)
+
+model = SimpleGCN(hidden_units=32, num_classes=num_classes, dropout_rate=0.2)
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
+
+epochs = 200
 for epoch in range(epochs):
     with tf.GradientTape() as tape:
         logits = model(X, A_norm, training=True)
+        logits_train = tf.gather(logits, train_indices)
+        y_train = tf.gather(y, train_indices)
+        loss = loss_fn(y_train, logits_train)
 
-       
-        train_indices_tf = tf.constant(train_indices, dtype=tf.int32)
-        test_indices_tf = tf.constant(test_indices, dtype=tf.int32)
-
-        
-        y_train_gathered = tf.gather(y, train_indices_tf)
-        logits_train_gathered = tf.gather(logits, train_indices_tf)
-
-        
-        loss = loss_fn(y_train_gathered, logits_train_gathered)
-
-    
     grads = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-    
-    train_preds = tf.argmax(logits_train_gathered, axis=1, output_type=tf.int32)
-    train_acc = tf.reduce_mean(tf.cast(train_preds == y_train_gathered, tf.float32))
+    if epoch % 20 == 0:
+        train_preds = tf.argmax(logits_train, axis=1)
+        acc = tf.reduce_mean(tf.cast(train_preds == y_train, tf.float32))
+        print(f"Epoch {epoch:03d} | Loss: {loss.numpy():.4f} | Train Acc: {acc.numpy():.4f}")
 
-    logits_test_gathered = tf.gather(logits, test_indices_tf)
-    y_test_gathered = tf.gather(y, test_indices_tf)
-    test_preds = tf.argmax(logits_test_gathered, axis=1, output_type=tf.int32)
-    test_acc = tf.reduce_mean(tf.cast(test_preds == y_test_gathered, tf.float32))
-
-    if epoch % 10 == 0:
-        print(f"Epoch {epoch:03d} | Loss: {loss.numpy():.4f} | Train Acc: {train_acc.numpy():.4f} | Test Acc: {test_acc.numpy():.4f}")
-
+logits_test = tf.gather(model(X, A_norm, training=False), test_indices)
+y_test = tf.gather(y, test_indices)
+test_preds = tf.argmax(logits_test, axis=1)
+test_acc = tf.reduce_mean(tf.cast(test_preds == y_test, tf.float32))
+print("Test Accuracy:", test_acc.numpy())
 
 
-final_preds = tf.argmax(model(X, A_norm, training=False), axis=1, output_type=tf.int32)
-print("Final predictions:", final_preds.numpy())
+my_vec = np.zeros(num_features, dtype=np.float32)  
+X_custom = tf.convert_to_tensor(my_vec.reshape(1,-1), dtype=tf.float32)
+A_custom = tf.constant([[1.0]], dtype=tf.float32)  
+
+logits = model(X_custom, A_custom, training=False)
+pred = tf.argmax(logits, axis=1).numpy()
+print("Prediction for custom vector:", pred)
